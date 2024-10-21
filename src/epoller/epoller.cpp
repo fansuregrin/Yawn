9664166447614943ab8b41add05b3eba93f44d3e
fansuregrin <pwz113436@gmail.com>
2024-10-21 21:09:52 +0800

    style: update doc comments and variable names

--- src/epoller/epoller.cpp
+++ src/epoller/epoller.cpp
@@ -4,94 +4,57 @@
  * @date 2024-03-24
  * @brief source file for epoller
 */
-#include <unistd.h>     // close
-#include <cassert>      // assert
 #include "epoller.h"
 
 
-/**
- * @brief 初始化一个 Epoller 实例
- * @param max_evnent_number 最大事件数量，默认为`1024`
-*/
-Epoller::Epoller(int max_event_number)
-: epoll_fd(epoll_create(512)), events(max_event_number) {
-    assert(epoll_fd >= 0 && events.size() > 0);
+Epoller::Epoller(int num_fds)
+: m_epoll_fd(epoll_create(512)), m_epoll_events(num_fds) {
+    if (m_epoll_fd < 0) {
+        LOG_ERROR("epoll_create failed: %s", strerror(errno));
+        exit(EXIT_FAILURE);
+    }
 }
 
-/**
- * @brief 销毁一个 Epoller 实例
-*/
 Epoller::~Epoller() {
-    close(epoll_fd);
+    close(m_epoll_fd);
 }
 
-/**
- * @brief 为指定的文件描述符注册事件
- * @param fd 被注册事件的目标文件描述符
- * @param events 被注册的事件
- * @return 是否注册成功
-*/
 bool Epoller::add_fd(int fd, uint32_t events) {
     if (fd < 0) return false;
     epoll_event event;
     event.data.fd = fd;
     event.events = events;
-    return epoll_ctl(epoll_fd, EPOLL_CTL_ADD, fd, &event) == 0;
+    return epoll_ctl(m_epoll_fd, EPOLL_CTL_ADD, fd, &event) == 0;
 }
 
-/**
- * @brief 从 epollfd 标识的 epoll 内核事件表中修改 fd 上的注册事件
- * @param fd 被修改事件的目标文件描述符
- * @param events 新的事件
- * @return 是否修改成功
-*/
 bool Epoller::mod_fd(int fd, uint32_t events) {
     if (fd < 0) return false;
     epoll_event event;
     event.data.fd = fd;
     event.events = events;
-    return epoll_ctl(epoll_fd, EPOLL_CTL_MOD, fd, &event) == 0;
+    return epoll_ctl(m_epoll_fd, EPOLL_CTL_MOD, fd, &event) == 0;
 }
 
-/**
- * @brief 从 epollfd 标识的 epoll 内核事件表中删除 fd 上的所有注册事件，并关闭 fd
- * @param fd 目标文件描述符
- * @return 是否删除成功
-*/
 bool Epoller::del_fd(int fd) {
     if (fd < 0) return false;
     // Since Linux 2.6.9, event can be specified as NULL when using EPOLL_CTL_DEL. 
     // However, applications that need to be portable to kernels before 
     // Linux 2.6.9 should specify a non-null pointer in event.
     epoll_event event = {0};
-    return epoll_ctl(epoll_fd, EPOLL_CTL_DEL, fd, &event) == 0;
+    return epoll_ctl(m_epoll_fd, EPOLL_CTL_DEL, fd, &event) == 0;
 }
 
-/**
- * @brief 等待 I/O 事件
- * @param timeout 超时时间，单位为毫秒(milliseconds)
- * @return 为请求的 I/O 准备好的文件描述符的数量
-*/
 int Epoller::wait(int timeout) {
-    return epoll_wait(epoll_fd, &events[0], static_cast<int>(events.size()), timeout);
+    return epoll_wait(m_epoll_fd, &m_epoll_events[0], 
+        static_cast<int>(m_epoll_events.size()), timeout);
 }
 
-/**
- * @brief 获取有事件发生的文件描述符
- * @param idx 索引
- * @return 文件描述符
-*/
 int Epoller::get_event_fd(size_t idx) const {
-    assert(idx >= 0 && idx < events.size());
-    return events[idx].data.fd;
+    assert(idx >= 0 && idx < m_epoll_events.size());
+    return m_epoll_events[idx].data.fd;
 }
 
-/**
- * @brief 获取事件
- * @param idx 索引
- * @return 事件
-*/
 uint32_t Epoller::get_events(size_t idx) const {
-    assert(idx >= 0 && idx < events.size());
-    return events[idx].events;
+    assert(idx >= 0 && idx < m_epoll_events.size());
+    return m_epoll_events[idx].events;
 }
--- src/epoller/epoller.h
+++ src/epoller/epoller.h
@@ -7,29 +7,77 @@
 #ifndef EPOLLER_H
 #define EPOLLER_H
 
-#include <sys/epoll.h>
+#include <unistd.h>     // close
+#include <cassert>      // assert
+#include <cstring>      // strerror
+#include <sys/epoll.h>  // epoll_create, epoll_wait, epoll_ctl
 #include <vector>
+#include "../log/log.h"
 
 
-/**
- * @brief 
-*/
 class Epoller {
 public:
-    explicit Epoller(int max_event_number = 1024);
+    /**
+     * @brief Epoller 构造函数
+     * @param max_evnent_number 最大被监听的文件描述符数量，默认为 `1024`
+    */
+    explicit Epoller(int num_fds = 1024);
+    
+    /**
+     * @brief Epoller 析构函数
+    */
     ~Epoller();
-public:
+
+    /**
+     * @brief 为指定的文件描述符注册事件
+     * @param fd 被注册事件的目标文件描述符
+     * @param events 被注册的事件
+     * @return 是否注册成功
+    */
     bool add_fd(int fd, uint32_t events);
+    
+    /**
+     * @brief 从 epoll 内核事件表中修改 fd 上的注册事件
+     * @param fd 被修改事件的目标文件描述符
+     * @param events 新的事件
+     * @return 是否修改成功
+    */
     bool mod_fd(int fd, uint32_t events);
+    
+    /**
+     * @brief 从 epoll 内核事件表中删除 fd 上的所有注册事件
+     * @param fd 目标文件描述符
+     * @return 是否删除成功
+    */
     bool del_fd(int fd);
+    
+    /**
+     * @brief 等待就绪事件
+     * @param timeout 超时时间，单位为毫秒(milliseconds)
+     * @return 有就绪事件的文件描述符的数量
+    */
     int wait(int timeout);
+
+    /**
+     * @brief 获取有事件发生的文件描述符
+     * @param idx 索引
+     * @return 文件描述符
+    */
     int get_event_fd(size_t idx) const;
+    
+    /**
+     * @brief 获取事件
+     * @param idx 索引
+     * @return 事件
+    */
     uint32_t get_events(size_t idx) const;
+
 private:
     // a file descriptor referring to the new epoll instance
-    int epoll_fd;
-    // 存放 `epoll_event` 的容器
-    std::vector<struct epoll_event> events;
+    int m_epoll_fd;
+
+    // 存放 `epoll_event` 的数组
+    std::vector<struct epoll_event> m_epoll_events;
 };
 
 #endif // EPOLLER_H
